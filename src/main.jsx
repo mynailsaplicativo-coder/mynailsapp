10ba4295200a84b8ec7b95dccf9d97fce4740427
mestresdatattoocontato-netizen <262137535+mestresdatattoocontato-netizen@users.noreply.github.com>
2026-07-20 21:42:59 -0300

    chore: switch to Clerk Production keys

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App.jsx'
 import './index.css'
 
-// Chave Pública do Clerk chumbada no código
-const PUBLISHABLE_KEY = 'pk_test_bGFyZ2UtZWFyd2lnLTAuY2xlcmsuYWNjb3VudHMuZGV2JA'
+// Chave Pública do Clerk (Produção)
+const PUBLISHABLE_KEY = 'pk_live_Y2xlcmsubXluYWlsc2FwcC5jb20uYnIk'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
